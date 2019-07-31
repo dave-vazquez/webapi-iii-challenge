8ad8c482929adcfc76afd707ed90a79b12cdcb0e
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-30 22:17:48 -0400

    mounted usersRouter to uri /api/users

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const postsRouter = require('./posts/postRouter');
+const usersRouter = require('./users/userRouter');
 const server = express();
 
 server.use(express.json());
 server.use(logger);
 server.use('/api/posts', postsRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
