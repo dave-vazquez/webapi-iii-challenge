07af3561627609d20c666a6ed823d2c8b7392031
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-30 21:54:31 -0400

    implemented validateRequestBody middleware and handler for PUT request at uri api/posts/:id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const postDb = require('./postDb');
 const router = express.Router();
-
+// router.use(express.json());
 router.get('/', (req, res) => {
   postDb
     .get()
@@ -44,7 +44,26 @@ router.delete('/:id', validatePostId, (req, res) => {
     });
 });
 
-router.put('/:id', validatePostId, (req, res) => {});
+router.put('/:id', validatePostId, validateRequestBody, (req, res) => {
+  const id = req.params.id;
+  const post = req.body;
+
+  res.status(200);
+  postDb
+    .update(id, post)
+    .then(post => {
+      res.status(200).json({
+        success: true,
+        post
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        err
+      });
+    });
+});
 
 //custom middleware
 async function validatePostId(req, res, next) {
@@ -66,6 +85,18 @@ async function validatePostId(req, res, next) {
       success: false,
       err
     });
+  }
+}
+
+function validateRequestBody(req, res, next) {
+  const post = req.body;
+  if (!post.text || !post.user_id) {
+    res.status(400).json({
+      success: false,
+      message: 'Please provide text and a user id.'
+    });
+  } else {
+    next();
   }
 }
 
