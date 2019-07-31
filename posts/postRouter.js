6da9df2fe2ca8ff82aeff196c1fd81ca95f196c6
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-30 21:33:08 -0400

    implmented handler for DELETE reqest on uri api/posts/:id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -26,7 +26,23 @@ router.get('/:id', validatePostId, (req, res) => {
   });
 });
 
-router.delete('/:id', validatePostId, (req, res) => {});
+router.delete('/:id', validatePostId, (req, res) => {
+  const id = req.post.id;
+  postDb
+    .remove(id)
+    .then(post => {
+      res.status(200).json({
+        success: true,
+        post
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: 'Could not remove post.'
+      });
+    });
+});
 
 router.put('/:id', validatePostId, (req, res) => {});
 
@@ -42,7 +58,7 @@ async function validatePostId(req, res, next) {
     } else {
       res.status(404).json({
         success: false,
-        message: 'Could not find a Post by that id.'
+        message: 'Could not find a post by that id.'
       });
     }
   } catch (err) {
