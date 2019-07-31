bb94319964c0d82498d1d6f8c3d8d4c19ecf431a
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-30 21:26:15 -0400

    implemented validatePostID middleware and handler for GET request

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,56 @@
-const express = 'express';
-
+const express = require('express');
+const postDb = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  postDb
+    .get()
+    .then(posts => {
+      res.status(200).json({
+        success: true,
+        posts
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        err
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+  res.status(200).json({
+    success: true,
+    post: req.post
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {});
 
-});
+router.put('/:id', validatePostId, (req, res) => {});
 
-router.put('/:id', (req, res) => {
+//custom middleware
+async function validatePostId(req, res, next) {
+  try {
+    const id = req.params.id;
+    const post = await postDb.getById(id);
 
-});
-
-// custom middleware
-
-function validatePostId(req, res, next) {
-
-};
+    if (post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'Could not find a Post by that id.'
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err
+    });
+  }
+}
 
 module.exports = router;
