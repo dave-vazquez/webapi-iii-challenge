fa0a35df9799aca373ae19cf2e5c72e306dbe1b2
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-01 23:14:32 -0400

    switched back to ternary assignment for env variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./server');
 require('dotenv').config();
 
-const port = process.env.PORT || 5000; //check if this works
+const port = process.env.PORT ? process.env.PORT : 5000; //check if this works
 
 server.listen(port, () => {
   console.log(`listening on port ${port}.`);
