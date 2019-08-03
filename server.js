95d719ba943bb5b75738ea86f66d2997c0e82fec
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-03 15:09:32 -0400

    reverted back to original GET handler

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ server.use('/api/posts', postsRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.send(`
+    <h2>${motd}</h2>
+    <p>Users - api/users<p>
+    <p>Posts - api/posts<p>
+    `);
 });
 
 //custom middleware
