05e8da70f4529f611a2fe9c5be3b7614e7ffc9d1
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-03 23:34:39 -0400

    added cors and swagger middleware

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require('express');
+const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const specs = require('./swaggerDoc');
 const postsRouter = require('./posts/postRouter');
 const usersRouter = require('./users/userRouter');
 const server = express();
 
 const motd = process.env.MOTD ? process.env.MOTD : 'No message today';
 
+// CORS middle-Ware
+server.use(cors());
+
+// Express Middle-Ware
 server.use(express.json());
+
+// Logger Middle-Ware
 server.use(logger);
+
+// Routes
 server.use('/api/posts', postsRouter);
 server.use('/api/users', usersRouter);
 
-server.get('/', (req, res) => {
-  res.send(`
-    <h2>${motd}</h2>
-    <p>Users - api/users<p>
-    <p>Posts - api/posts<p>
-    `);
-});
+// Swagger-UI Middle-Ware
+server.use('/assets', express.static('assets'));
+server.use(
+  '/',
+  swaggerUI.serve,
+  swaggerUI.setup(specs, {
+    customCss: '.swagger-ui .topbar {display: none',
+    customSiteTitle: 'Lambda Posts',
+    customfavIcon: './assets/favicon.ico'
+  })
+);
 
-//custom middleware
+/********************************************************
+ *                  LOGGER MIDDLE-WARE                  *
+ ********************************************************/
 function logger(req, res, next) {
   console.log(`method: ${req.method}`);
   console.log(`path: ${req.url}`);
